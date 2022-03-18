0328371328e8b2b74bc1f8aa31a52dfefeb935a4
Himani <himani9.engg@gmail.com>
2022-03-18 21:04:35 +0530

    pattern2

--- OnlineShopping/src/main/java/Pattern2.java
+++ OnlineShopping/src/main/java/Pattern2.java
@@ -4,8 +4,8 @@ public class Pattern2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i=0,j=0,row=6;
-		for(i=0;i<row;i++) {
-			for(j=0;j<i;j++) {
+		for(i=row;i>0;i--) {
+			for(j=i;j>0;j--) {
 				System.out.print('*');
 			}
 			System.out.println();
