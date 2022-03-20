2e075c02843fb4dda528c4e26306c1cc830f603a
Himani <himani9.engg@gmail.com>
2022-03-20 16:08:34 +0530

    ArrayList methods

--- OnlineShopping/src/main/java/CollectionList.java
+++ OnlineShopping/src/main/java/CollectionList.java
@@ -10,7 +10,22 @@ public class CollectionList {
 		l.add("apple");
 		System.out.println(l);
 		l.add(1, "mango");
+		l.add(2,15);
 		System.out.println(l);
+		l.remove(3);
+		l.iterator();
+		System.out.println(l);
+		
+		List l1=new ArrayList();
+		l1.addAll(l);
+		System.out.println(l1);
+		Boolean b=l1.contains("mango");
+		System.out.println(b);
+		l1.clear();
+		System.out.println(l1);
+		List l2=new ArrayList();
+		Boolean b1=l1.equals(l2);
+		System.out.println(b1);
 	}
 
 }
